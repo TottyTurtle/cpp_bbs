9a1885e12c05096e70bc8b105249885d625975f5
TottyTurtle <totty@jentzsch-online.de>
2023-06-09 18:54:57 +0200

    Snake

--- Aufgabe - Snake/main.cpp
+++ Aufgabe - Snake/main.cpp
@@ -9,11 +9,13 @@
 using namespace std;
 using namespace std::this_thread;
 
-int size_x=15, size_y=30,
-    point_x=1, point_y=1,
-    direction, last_direction,
-    snake_size = 10;
-int snake[15][30][2];
+string debug = "";
+int size_x=15, size_y=30,       //Spielfeld
+    point_x=1, point_y=1,       //Postion
+    direction, last_direction,  //Richtung
+    snake_size = 10,            //Snake länge
+    snake[15][30][2];          //Snake
+
 
 int getKey()
 {
@@ -22,6 +24,19 @@ int getKey()
       result = 256 + getch();
    }
    return result;
+}
+
+void setFood() {
+renewfood:
+    int posx = rand() % size_x,
+        posy = rand() % size_x;
+
+    if(snake[posx][posy][0] > 0) {
+        goto renewfood;
+    }
+    snake[posx][posy][0] = 1;
+    snake[posx][posy][1] = -1;
+
 }
 
 string getHead() {
@@ -86,6 +101,7 @@ string getBodySign(int number) {
 void layout() {
     system("cls");
     string top = "";
+    int snake_add = 0;
     for(int x=0;x<size_y;x++) {
         top += "═";
     }
@@ -94,7 +110,15 @@ void layout() {
         cout << "║";
         for(int y=0;y<size_y;y++) {
             if(point_x == x && point_y == y) {
+                if(snake[x][y][1] == -1) {
+
+                    snake[x][y][1] = 0;
+                    snake_add = 1;
+                    setFood();
+                }
                 cout << getHead();
+            } else if(snake[x][y][1] == -1) {
+                cout << "O";
             } else if(snake[x][y][0] > 0) {
                 if(snake[x][y][0] == snake_size) {
                     snake[x][y][1] = getBody();
@@ -108,11 +132,14 @@ void layout() {
         cout << "║" << endl;
     }
     cout << "╚" << top << "╝" << endl;
-    cout << point_x << " " << point_y;
+    cout << point_x << " " << point_y << " Debug: " << debug;
+
+    snake_size += snake_add;
     snake[point_x][point_y][0] = snake_size;
 }
 int main() {
     system("chcp 65001 > nul"); //UTF-8 support
+    srand (time(NULL));
 
     direction = 1;
     last_direction = 1;
@@ -123,7 +150,7 @@ int main() {
               snake[x][y][1] = 0;
         }
     }
-
+    setFood();
     while(true) {
         layout();
 
@@ -166,7 +193,33 @@ int main() {
                 point_y++;
                 break;
         }
+        if(point_x >= size_x) {
+            point_x = 0;
+        } else if(point_x < 0) {
+            point_x = size_x - 1;
+        }
+        if(point_y >= size_y) {
+            point_y = 0;
+        } else if(point_y < 0) {
+            point_y = size_y - 1;
+        }
+        debug = to_string(snake[point_x][point_y][1]) + "";
 
+        if(snake[point_x][point_y][0] > 0) {
+            int num = snake[point_x][point_y][0];
+            for(int x=0;x<size_x;x++) {
+                for(int y=0;y<size_y;y++) {
+                    if(snake[x][y][1] < 0) {
+                        continue;
+                    }
+                    if(snake[x][y][0] < num) {
+                        snake[x][y][0] = 0;
+                        snake[x][y][1] = 0;
+                    }
+
+                }
+            }
+        }
 
         sleep_for(60ms);
     }
