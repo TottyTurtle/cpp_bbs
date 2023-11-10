86b5297bc9bde4153f2fdf91b99e0d0d85ebdadb
TottyTurtle <totty@jentzsch-online.de>
2023-11-10 15:42:21 +0100

    Update

--- Übungsklausur 5 - HTML/main.cpp
+++ Übungsklausur 5 - HTML/main.cpp
@@ -11,13 +11,27 @@ int main() {
     int zeilen;
     cin >> zeilen;
 
+
+    cout << "Wie viele Spalten soll die Tabelle haben?" << endl;
+
+    int spalte;
+    cin >> spalte;
+
     ofstream fout;
     fout.open("index.html",ios::out);
     fout << "<html>\n\t<header></header>\n\t<body>";
-    fout << "\n\t<table border=\"4\">";
+
+    fout << "\n\t\t<table border=\"4\">";
+
     for(int i=1;i<=zeilen;i++) {
-        fout << "\n\t\t<tr><td>Zeile " << i << "</td></tr>";
+        fout << "\n\t\t\t<tr>";
+        for(int j=1;j<=spalte;j++) {
+            fout << "\n\t\t\t\t<td>Zeile " << i << ", Spalte " << j << "</td>";
+        }
+        fout << "\n\t\t\t</tr>";
     }
+
+    fout << "\n\t\t</table>";
     fout << "\n\t</body>\n</html>";
     fout.close();
 
